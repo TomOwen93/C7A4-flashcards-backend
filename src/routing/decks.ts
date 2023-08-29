2868e42136a0fb139d264b7b08f12d0ad4f4289e
TomOwen93 <mr.tsowen@gmail.com>
2023-08-29 16:21:32 +0100

    add new user, new deck

--- src/routing/decks.ts
+++ src/routing/decks.ts
@@ -44,4 +44,20 @@ export function addDeckRoutes(app: Express, client: Client) {
             );
         }
     });
+
+    app.post("/decks/", async (req, res) => {
+        try {
+            const query =
+                "INSERT INTO decks(name, userid) values ($1, $2) RETURNING *";
+            const values = [req.body.name, req.body.userid];
+            const response = await client.query(query, values);
+            const data = response.rows[0];
+
+            res.status(200).json(data);
+        } catch (error) {
+            res.status(500).json(
+                `request ${req.url} caused error, check server logs.`
+            );
+        }
+    });
 }
--- src/routing/users.ts
+++ src/routing/users.ts
@@ -19,9 +19,9 @@ export function addUserRoutes(app: Express, client: Client) {
 
     app.get("/users", async (req, res) => {
         try {
-            const query = "";
-            const values = [req.params];
-            const response = await client.query(query, values);
+            const query = "SELECT * FROM users";
+
+            const response = await client.query(query);
             const data = response.rows;
             res.status(200).json(data);
         } catch (error) {
@@ -37,6 +37,22 @@ export function addUserRoutes(app: Express, client: Client) {
             const values = [req.params.userid];
             const response = await client.query(query, values);
             const data = response.rows;
+            res.status(200).json(data);
+        } catch (error) {
+            res.status(500).json(
+                `request ${req.url} caused error, check server logs.`
+            );
+        }
+    });
+
+    app.post<{ username: string }>("/users/", async (req, res) => {
+        try {
+            const query = "INSERT INTO users(username) values($1) returning *;";
+            const values = [req.body.username];
+
+            const response = await client.query(query, values);
+            const data = response.rows[0];
+            console.log(data);
             res.status(200).json(data);
         } catch (error) {
             res.status(500).json(
