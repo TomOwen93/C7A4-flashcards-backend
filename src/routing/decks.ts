1384e056d8760b87b2eeb7d075541c2682fd798d
henrypgill <61098944+henrypgill@users.noreply.github.com>
2023-08-29 13:00:09 +0100

    broke routing out into seperate files and functions

---
+++ src/routing/decks.ts
@@ -0,0 +1,47 @@
+import { Client } from "pg";
+import express from "express";
+
+type Express = ReturnType<typeof express>;
+
+export function addDeckRoutes(app: Express, client: Client) {
+    /* ------- template for /decks request methods -------*/
+    // app.get("/decks/", async (req, res) => {
+    //     try {
+    //         const query = "";
+    //         const values = [req.params];
+    //         const response = await client.query(query, values);
+    //         const data = response.rows
+    //         res.status(200).json(data)
+    //     } catch (error) {
+    //         res.status(500).json(`request ${req.url} caused error, check server logs.`);
+    //     }
+    // })
+
+    app.get("/decks", async (req, res) => {
+        try {
+            const query = "SELECT * FROM DECKS;";
+            // const values = [""];
+            const response = await client.query(query);
+            const data = response.rows;
+            res.status(200).json(data);
+        } catch (error) {
+            res.status(500).json(
+                `request ${req.url} caused error, check server logs.`
+            );
+        }
+    });
+
+    app.get("/decks/:userid", async (req, res) => {
+        try {
+            const query = "SELECT * FROM decks WHERE userid=$1";
+            const values = [req.params.userid];
+            const response = await client.query(query, values);
+            const data = response.rows;
+            res.status(200).json(data);
+        } catch (error) {
+            res.status(500).json(
+                `request ${req.url} caused error, check server logs.`
+            );
+        }
+    });
+}
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import express from "express";
 import { Client } from "pg";
 import { getEnvVarOrFail } from "./support/envVarUtils";
 import { setupDBClientConfig } from "./support/setupDBClientConfig";
+import { addDeckRoutes } from "./routing/decks";
+import { addUserRoutes } from "./routing/users";
+import { addGeneralRoutes } from "./routing/general";
 
 dotenv.config(); //Read .env file lines as though they were env vars.
 
@@ -16,45 +19,9 @@ const app = express();
 app.use(express.json()); //add JSON body parser to each following route handler
 app.use(cors()); //add CORS support to each following route handler
 
-app.get("/users", async (_req, res) => {
-    try {
-        const queryText = "SELECT * FROM USERS;";
-        const result = await client.query(queryText);
-
-        res.status(200).json(result.rows);
-    } catch (error) {
-        //Recover from error rather than letting system halt
-        console.error(error);
-        res.status(500).send("An error occurred. Check server logs.");
-    }
-});
-
-app.get("/health-check", async (_req, res) => {
-    try {
-        //For this to be successful, must connect to db
-        await client.query("select now()");
-        res.status(200).send("system ok");
-    } catch (error) {
-        //Recover from error rather than letting system halt
-        console.error(error);
-        res.status(500).send("An error occurred. Check server logs.");
-    }
-});
-
-app.get("/decks/:userid", async (req, res) => {
-    try {
-        const query = "SELECT * FROM DECKS WHERE userid=$1"; //JOIN users ON users.userid=decks.userid WHERE userid=$1;";
-        const userId = req.params.userid;
-        const decks = await client
-            .query(query, [userId])
-            .then((response) => response.rows);
-        res.status(200).json(decks);
-    } catch (error) {
-        //Recover from error rather than letting system halt
-        console.error(error);
-        res.status(500).json("An error occurred. Check server logs.");
-    }
-});
+addDeckRoutes(app, client);
+addUserRoutes(app, client);
+addGeneralRoutes(app, client);
 
 connectToDBAndStartListening();
 
